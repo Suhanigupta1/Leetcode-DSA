8e46102170361fbcf3fc722880a387aee7452d9d
Suhani Gupta <114853473+Suhanigupta1@users.noreply.github.com>
2024-04-23 15:19:28 +0530

    Time: 18 ms (20.71%) | Memory: 49.4 MB (5.56%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null && strs.length==0){
+            return new ArrayList<>();
+        }
+        HashMap<String, ArrayList<String>> ans = new HashMap<>();
+        for(String str: strs){
+            int[] ch = new int[26];
+            for(char ch2 :str.toCharArray()){
+                ch[ch2-'a']++;
+            }
+            String keystr = Arrays.toString(ch);
+            if(!ans.containsKey(keystr)){
+                ans.put(keystr, new ArrayList<>());
+            }
+            ans.get(keystr).add(str);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
