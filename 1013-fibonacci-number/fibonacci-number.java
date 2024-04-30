2b189b409183b3f24bb6f4f4fc0f9b36888b6224
Suhani Gupta <114853473+Suhanigupta1@users.noreply.github.com>
2024-04-30 18:10:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (72.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,18 +1,15 @@
 import java.util.*;
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return getFib(dp, n);    
+    int[] dp = new int[n+1];
+    dp[0] = 0;
+    if(n>=1){
+        dp[1] = 1;
     }
-    public int getFib(int[] dp, int n){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]==-1){
-            dp[n] = getFib(dp, n-1)+getFib(dp, n-2);
-        }
-        return dp[n];
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1]+dp[i-2];
     }
-
+    
+    return dp[n];
+    }
 }
